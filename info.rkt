69c2f55d3eb61d8294f459ccec71fbcf74463f5d
Avelino <avelinorun@gmail.com>
2022-01-12 08:47:51 -0300

    fixed ident (tab)
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- info.rkt
+++ info.rkt
@@ -1,13 +1,15 @@
 #lang info
 (define version "0.1")
 (define collection 'multi)
-(define deps '(["base" #:version "6.1"]
-               "html-lib"
-               ["markdown" #:version "0.25"]
-               "racket-index"
-               "web-server-lib"))
-(define build-deps '("at-exp-lib"
-                     "net-doc"
-                     "racket-doc"
-                     "rackunit-lib"
-                     "web-server-doc"))
+(define deps '(
+    ["base" #:version "6.1"]
+    "html-lib"
+    ["markdown" #:version "0.25"]
+    "racket-index"
+    "web-server-lib"))
+(define build-deps '(
+    "at-exp-lib"
+    "net-doc"
+    "racket-doc"
+    "rackunit-lib"
+    "web-server-doc"))
--- main.rkt
+++ main.rkt
@@ -1,12 +1,12 @@
 #lang racket
 
 (require markdown
-	 racket/file
-	 2htdp/batch-io)
+	racket/file
+	2htdp/batch-io)
 
 (define readmemd (parse-markdown (file->string "./README.md")))
 (define markdown2html (xexpr->string `
-		    (html ()
-			  (head () (title "A curated list of awesome Racket frameworks, libraries and software, maintained by Community - Awesome Racket"))
-			  (body () ,@readmemd))))
+	(html ()
+		(head () (title "A curated list of awesome Racket frameworks, libraries and software, maintained by Community - Awesome Racket"))
+		(body () ,@readmemd))))
 (write-file "tmp/index.html" markdown2html)
